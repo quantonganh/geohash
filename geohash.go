272db470ce83c302a1ed32fb1a2ac45616238326
Quan Tong <quantonganh@gmail.com>
2023-12-20 09:48:43 +0700

    add func to calculate distance between 2 lat/lng points

--- geohash.go
+++ geohash.go
@@ -189,3 +189,28 @@ func EstimateLengthRequired(r float64) int {
 
 	return l
 }
+
+// https://www.movable-type.co.uk/scripts/latlong.html
+func Distance(lat1, lng1, lat2, lng2 float64) float64 {
+	lat1Rad := degreesToRadians(lat1)
+	lng1Rad := degreesToRadians(lng1)
+	lat2Rad := degreesToRadians(lat2)
+	lng2Rad := degreesToRadians(lng2)
+
+	dLat := lat2Rad - lat1Rad
+	dLng := lng2Rad - lng1Rad
+
+	// Haversine formula
+	a := math.Sin(dLat/2)*math.Sin(dLat/2) +
+		math.Cos(lat1Rad)*math.Cos(lat2Rad)*
+			math.Sin(dLng/2)*math.Sin(dLng/2)
+	c := 2 * math.Atan2(math.Sqrt(a), math.Sqrt(1-a))
+
+	distance := earthRadius * c
+
+	return distance
+}
+
+func degreesToRadians(degrees float64) float64 {
+	return degrees * (math.Pi / 180.0)
+}
